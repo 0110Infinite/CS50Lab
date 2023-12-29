63f41c7197c30f7bfcf850e27fcb3e50c47d7cb9
0110Infinite <151993288+0110Infinite@users.noreply.github.com>
2023-12-29 16:00:10 +0800

    Update Mario-less.c

--- WEEK1/Mario-less.c
+++ WEEK1/Mario-less.c
@@ -10,11 +10,11 @@ int main(void)
     }
     while (n < 1 || n > 8);
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)//打印一竖列#
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)//打印一横行#
         {
-            if (j + i >= n - 1)
+            if (j + i >= n - 1)//满足要打印图形条件的坐标系构成的函数
             {
                 printf("#");
             }
